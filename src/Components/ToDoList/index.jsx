9d8abe9649be781b8c925bd90e32ecf20aaf8a4f
archiebald777 <archiechekatouski777@gmail.com>
2021-10-14 21:01:14 +0300

    add select function and regex

--- src/Components/ToDoList/index.jsx
+++ src/Components/ToDoList/index.jsx
@@ -1,77 +1,65 @@
 import { Form, Field, Formik, ErrorMessage } from "formik";
-import React from "react";
+import React, { useCallback } from "react";
 import { useState } from "react";
-
-
-const data = [
- 
-  ]
-
+import { TODO_SCHEMA } from "./Validation";
+const data = [];
 
 const ToDoList = () => {
-const [tasks, setTasks] = useState(data);
+  const [tasks, setTasks] = useState(data);
 
+  const addTask = (text) => {
+    const newTask = {
+      id: Date.now(),
+      isDone: false,
+      body: text,
+    };
 
+    const newTasks = [...tasks, newTask];
 
+    setTasks(newTasks);
+  };
 
+  const deleteTask = (id) => {
+    const filter = tasks.filter((task) => task.id !== id);
+    return setTasks(filter);
+  };
 
+  const [completedTasks, setCompetedTasks] = useState([]);
+  const toggleCompletion = (taskId) => {
+    const newCompletedTaskList = [...completedTasks];
+    if (completedTasks.indexOf(taskId) != -1) {
+    } else {
+      newCompletedTaskList.push(taskId);
+    }
+    setCompetedTasks(newCompletedTaskList);
+  };
 
-
-
-const addTask = (text) => {
-  const newTask = {
-  id: Date.now(),
-  isDone: false,
-  body: text,
-};
-
-const newTasks = [...tasks, newTask];
-
-setTasks(newTasks);
-}
-
-const deleteTask = (id) => {
-const filter = tasks.filter(task => 
-  task.id !== id
-)
-return setTasks(filter)
-}
-
-
-const toggleCompletion = (checkbox) => {
- 
-}
-
-
-
-const submitHandler = (values, formikBag) => {
-  const {taskText} = values;
-  addTask(taskText);
-  formikBag.resetForm()
-}
+  const submitHandler = (values, formikBag) => {
+    const { taskText } = values;
+    addTask(taskText);
+    formikBag.resetForm();
+  };
 
   return (
     <div>
       <h1>To Do List</h1>
-      <Formik initialValues = {
-         {taskText: ''}
-      } onSubmit = {submitHandler}>
-<Form>
-  <Field name="taskText" placeholder="Enter new task"/>
-  <ErrorMessage name="taskText">
-    {(message) => <div style={{color:'red'}}>{message}</div>}
-  </ErrorMessage>
-</Form>
+      <Formik validationSchema={TODO_SCHEMA} initialValues={{ taskText: "" }} onSubmit={submitHandler}>
+        <Form>
+          <Field name="taskText" placeholder="Enter new task" />
+          <ErrorMessage name="taskText">
+            {(message) => <div style={{ color: "red" }}>{message}</div>}
+          </ErrorMessage>
+        </Form>
       </Formik>
       <ul>
         {tasks.map((task) => {
-          return <li>
-            {task.body}
-            <button onClick={() => deleteTask(task.id)}> Delete Task</button>
-            <Field name="checkbox" type="checkbox"/>
-          </li>
-          
-      
+          return (
+            <li>
+              {task.body}
+              <button onClick={() => deleteTask(task.id)}> Delete Task</button>
+              <input type="checkbox" />
+            </li>
+          );
         })}
       </ul>
     </div>
